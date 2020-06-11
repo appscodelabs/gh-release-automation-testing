ba0aeba1569705cda7afc1203ab93022707b3a8e
Tamal Saha <tamal@appscode.com>
2020-06-10 17:23:20 -0700

    added-all

--- parse-release-tracker.sh
+++ parse-release-tracker.sh
@@ -4,7 +4,9 @@
 # msg=$(echo $msg)
 # echo "|" $msg "|"
 
-while IFS=$'\n: ' read -r line cmd; do
-    echo "$line"
-    echo "$cmd"
+while IFS=$': \t' read -r line cmd; do
+    if [ "$line" = "Release-tracker" ]; then
+        echo "$line"
+        echo "$cmd"
+    fi
 done < <(git tag -l --format='%(body)' v0.1.0)
